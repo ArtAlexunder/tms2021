5fd8b6b9337022e0028a00b814d25a56491ac103
ArtAlexunder <AlexanderArtemenko@coherentsolutions.com>
2021-11-03 12:43:27 +0300

    Work with collections

--- collection_.py
+++ collection_.py
@@ -17,3 +17,34 @@ coll_2 = ['hello', 'world', 'my', 'name']
 k.extend(coll_2)
 k.insert(5, 123)
 print(k)
+
+# Set
+
+my_set = {1, 1, 1, 2, 3, 4, 5}
+print(my_set)
+my_set.add(6)
+print(my_set)
+my_set.remove(1)
+print(my_set)
+
+set_a = {1, 2, 3, 4}
+set_b = {8, 7, 6, 5, 4, 3}
+print()
+print('set_a: ', set_a)
+print('set_b: ', set_b)
+print('| for set: ', set_a | set_b)
+print('& for set: ', set_a & set_b)
+print('^ for set: ', set_a ^ set_b)
+print('- for set: ', set_a - set_b)
+
+# Dictionary
+
+my_dict = {1: 'one', 2: 'two', 3: 'three'}
+print(my_dict)
+my_dict[4] = 'four'
+print(my_dict)
+my_dict[1] = 1
+print(my_dict)
+my_dict.pop(4)
+print(my_dict)
+
